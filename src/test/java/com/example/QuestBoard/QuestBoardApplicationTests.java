75b10c5de8666a36012c997b9e8dedb3038b0c17
917_Timofte_Razvan <92137529+TimofteRazvan@users.noreply.github.com>
2023-04-06 00:48:35 +0300

    Initial Commit.

---
+++ src/test/java/com/example/QuestBoard/QuestBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuestBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
